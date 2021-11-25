de1ca79950cfb88b4379368c530c35d87e94d7ed
Rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2021-11-25 22:29:46 +0530

    added comments to the code

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 
 
-
+//create a intial array of height for the bar 
 let a = [90,80,120,130,90,120,120,90,70,98,76,123,64,97,133,76,86,93,102,90,80,132,120,160,92,120,120,90,70,98,]
 
-
+//get the button where the audio need to play or  and maint a state need to chec where the audio is playing
 let btn = document.getElementById('btn');
-
 let runState = false;
-
-
-
-
+//get audio file form the html 
 let audio = document.getElementById('audio');
 
      btn.addEventListener('click', ()=> {
@@ -29,15 +25,29 @@ let audio = document.getElementById('audio');
         
     });
 
+    //get the canvas elemet for the mainuplation on click
+
+    let canvas = document.getElementById("canvas");
+    canvas.addEventListener('click',function(e){
+
+        getCursorPosition(canvas,e)
+
+    })
+
+
+    //this function will help to interact with the canvas and abil to get the postion of the cursor position 
+    
     function getCursorPosition(canvas, event) {
+
+        // get the bounders of the canvas
         const rect = canvas.getBoundingClientRect()
         const x = event.clientX-rect.left;
         const right = rect.right-rect.left; 
-        console.log("x: " + x)
-        console.log("Right " + right);
+
+        //and we need to converte into prectage for to use with play time with the music time 
+        // we manuplate the button to play and continue 
         let prectage = Math.floor((x/right)*100);
         let timeToSet = Math.floor((audio.duration * prectage)/100);
-        console.log(Math.floor((audio.duration * prectage)/100));
         audio.currentTime = timeToSet;
         audio.play();
         btn.innerHTML = `<i class="fas fa-pause"></i>`;
@@ -47,110 +57,92 @@ let audio = document.getElementById('audio');
 
     }
 
-    let canvas = document.getElementById("canvas");
-    canvas.addEventListener('click',function(e){
+   
 
-        getCursorPosition(canvas,e)
-
-    })
-
+    //to draw in the canvas we need to create context and the context is 2d
+    //context is used alot in place so i used c for simple and easy to use
     let c =canvas.getContext("2d");
 
 
+    //getting the bounders for the cavnas for the mainuplation
+
+    const rect = canvas.getBoundingClientRect()
+    const x = rect.left;
+    const right = rect.right-rect.left;
+    const height = Math.floor(rect.bottom-rect.top);
 
 
-const rect = canvas.getBoundingClientRect()
-        const x = rect.left;
-        const right = rect.right-rect.left;
-        const height = Math.floor(rect.bottom-rect.top);
-        console.log("top: " + rect.top+ " bottom: " + rect.bottom);
-        console.log("height: " + height);
-        console.log(right)
-
-
-// update the current time 
+    //this function run where time update on the of the audio plays but this we create a animation t
+    // effect for the canvas 
     audio.ontimeupdate = function() {
 
-        console.log("current time :" + audio.currentTime)
+       
         let currentTime = Math.floor(audio.currentTime);
         let min = String(Math.floor(currentTime/60)).padStart(2, '0')
         let sec = String(Math.floor(currentTime%60) ).padStart(2, '0')
         let dur = Math.floor(audio.duration)
         
+
+        // updating the time to the elelemet every time it update
         document.getElementById("crtTime").innerText = ("Timer min "+ min+" : sec " + sec);
         document.getElementById("durTime").innerText = ("Duratin min " + String(Math.floor(dur/60)).padStart(2, '0') + " : sec " + String(Math.floor(dur%60) ).padStart(2, '0'));
     
-          
-
-        console.log("muqic duratin: "+ dur);
-        console.log(`length  :  ${Math.floor((right - x)/dur)}`)
-        console.log(`width of the canvas :${ right - x} ` );
-        
+        //this will clear the previceos animation which we are added 
         c.clearRect(0,0,innerWidth,innerHeight);
+
+        //tag function draw a statci tag in the canvavs check funtion belwo code
         tag(10,70, "green", "Introduction");
         tag(80,70 , 'green', "once_sce");
         tag (180,70, "purple", "Rappport Bulling")
         tag(260,70,"brown" ,"Polite");
 
+        //this loop draw reactangle with gray color bar for the remaining time as to be play 
         for(let i = currentTime+1; i< dur; i++){
              c.fillStyle = "rgba(128,128,128,0.5)";
             c.fillRect(i*10,a[i]+20,4, -30);
-            
-        
         }
 
-
+        
+        // this loop draw reactangle with pink color var for the time has been played
         for(let i = 0 ; i<= currentTime; i++){
-
-            console.log(i*10)
             c.fillStyle = "rgba(255,0,0,0.5)"
             c.fillRect(i*10,a[i]+20,4,-30);
 
         }
         
-
-    }
-
-    for(let i = 0 ; i<= 30; i++){
-
-        console.log(i*10)
         
 
-        c.fillStyle = "rgba(128,128,128,0.5)";
-        c.fillRect(i*10,a[i]+20,4,-30);
-
     }
 
-
-
-function tag(x,y,color,text){
-
-  
-
-    c.fill()
-    c.beginPath();
-    
-    c.arc(x,y,2,0,2*Math.PI);
-    c.strokeStyle = color;
-    c.fill()
-    c.stroke();
-    
-    
-    c.fillStyle = color;
-    c.fillRect(x,y,1,-40);
-    c.fillRect(x-10, y-45,20,5);
-
-    c.font = '10px Arial';
-    c.fillStyle = "black";
-    c.fillText(text,x-10,y-45)
-
+    //for inital rendering the this function wil be draw a gray bar chart
     
 
+    function draw(inital , end , color, arr){
+        for(let i = inital ; i<= end; i++){ 
+            c.fillStyle = color;
+            c.fillRect(i*10,arr[i]+20,4,-30);
+        }
+    }
+    draw(0,30,"rgba(128,128,128,0.5)", a)
 
+   //this function crearte a statci tags for the canvas 
+    function tag(x,y,color,text){
 
+        c.fill()
+        c.beginPath();
+        c.arc(x,y,2,0,2*Math.PI);
+        c.strokeStyle = color;
+        c.fill()
+        c.stroke();
+        c.fillStyle = color;
+        c.fillRect(x,y,1,-40);
+        c.fillRect(x-10, y-45,20,5);
+        c.font = '10px Arial';
+        c.fillStyle = "black";
+        c.fillText(text,x-10,y-45)
 
 }
-
+//calling the stack tags initally to render 
 tag(10,70, "green", "Introduction");
 tag(80,70 , 'green', "once_sce");
 tag (180,70, "purple", "Rappport Bulling")
